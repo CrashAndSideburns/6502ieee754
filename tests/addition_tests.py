27a949497e62d43b468bbcab1cb0e7d9ba14c0b1
Morgan Arnold <morgan.r.arnold@outlook.com>
2021-10-31 21:42:03 -0700

    Add near base tests
    Add tests testing addition of all values within a Hamming distance of 1 from base values.

--- tests/addition_tests.py
+++ tests/addition_tests.py
@@ -130,7 +130,7 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(self.ONE, self.NAN)
         self.run_test(-self.ONE, self.NAN)
 
-    def test_min_subnorm_addition(self):
+    def test_min_subnorm(self):
         # Test ±MIN_SUBNORM + x for all types of x.
         self.run_test(self.MIN_SUBNORM, self.ZERO)
         self.run_test(self.MIN_SUBNORM, -self.ZERO)
@@ -180,7 +180,7 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(self.MIN_SUBNORM, self.NAN)
         self.run_test(-self.MIN_SUBNORM, self.NAN)
 
-    def test_subnorm_addition(self):
+    def test_subnorm(self):
         # Test ±x + y for subnormal x and all types of y.
         self.run_test(numpy.float32(7.518523e-39), self.ZERO)
         self.run_test(numpy.float32(7.518523e-39), -self.ZERO)
@@ -230,7 +230,7 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(numpy.float32(3.974073e-39), self.NAN)
         self.run_test(-numpy.float32(3.974073e-39), self.NAN)
 
-    def test_max_subnorm_addition(self):
+    def test_max_subnorm(self):
         # Test ±MAX_SUBNORM + x for all types of x.
         self.run_test(self.MAX_SUBNORM, self.ZERO)
         self.run_test(self.MAX_SUBNORM, -self.ZERO)
@@ -280,7 +280,7 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(self.MAX_SUBNORM, self.NAN)
         self.run_test(-self.MAX_SUBNORM, self.NAN)
 
-    def test_min_norm_addition(self):
+    def test_min_norm(self):
         # Test ±MIN_NORM + x for all types of x.
         self.run_test(self.MIN_NORM, self.ZERO)
         self.run_test(self.MIN_NORM, -self.ZERO)
@@ -330,7 +330,7 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(self.MIN_NORM, self.NAN)
         self.run_test(-self.MIN_NORM, self.NAN)
 
-    def test_norm_addition(self):
+    def test_norm(self):
         # Test ±x + y for normal x and all types of y.
         self.run_test(numpy.float32(3.2528998e8), self.ZERO)
         self.run_test(numpy.float32(3.2528998e8), -self.ZERO)
@@ -380,7 +380,7 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(numpy.float32(2.2122593e-30), self.NAN)
         self.run_test(-numpy.float32(2.2122593e-30), self.NAN)
 
-    def test_max_norm_addition(self):
+    def test_max_norm(self):
         # Test ±MAX_NORM + x for all types of x.
         self.run_test(self.MAX_NORM, self.ZERO)
         self.run_test(self.MAX_NORM, -self.ZERO)
@@ -430,7 +430,7 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(self.MAX_NORM, self.NAN)
         self.run_test(-self.MAX_NORM, self.NAN)
 
-    def test_infinity_addition(self):
+    def test_infinity(self):
         # Test ±∞ + x for all types of x.
         self.run_test(self.INF, self.ZERO)
         self.run_test(self.INF, -self.ZERO)
@@ -480,7 +480,7 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(self.INF, self.NAN)
         self.run_test(-self.INF, self.NAN)
 
-    def test_nan_addition(self):
+    def test_nan(self):
         # Test ±NaN + x for all types of x.
         self.run_test(self.NAN, self.ZERO)
         self.run_test(self.NAN, -self.ZERO)
@@ -510,6 +510,227 @@ class TestBasicAddition(unittest.TestCase):
         self.run_test(self.NAN, -self.INF)
 
         self.run_test(self.NAN, self.NAN)
+
+class TestNearBaseAddition(unittest.TestCase):
+    # Test addition of floats separated by a minimal Hamming distance.
+
+    ZERO = numpy.float32(0)
+    ONE = numpy.float32(1)
+    MIN_SUBNORM = numpy.float32(1e-45)
+    MAX_SUBNORM = numpy.float32(1.1754942e-38)
+    MIN_NORM = numpy.float32(1.1754944e-38)
+    MAX_NORM = numpy.float32(3.4028235e38)
+
+    # Initialise the tester object used to run the assembled code.
+    @classmethod
+    def setUpClass(cls):
+        cls.tester = test_utils.SubroutineTester("test_addition.s")
+        cls.tester.initialise()
+
+    # Run a test by flipping one bit of the second float's mantissa and testing that the sum is correct.
+    def run_test(self, float1: numpy.float32, float2: numpy.float32, flip_bit: int):
+        # The code to flip one bit of the second float is kind of a mess.
+        float2 = numpy.frombuffer(bytes(map(lambda a, b: a ^ b, numpy.float32.tobytes(float2), numpy.int32.tobytes(numpy.int32(1 << flip_bit)))), dtype=numpy.float32)[0]
+        # After flipping a bit, proceed as normal.
+        print(float2)
+        expected = float1 + float2
+        if numpy.isnan(expected):
+            self.assertTrue(numpy.isnan(TestBasicAddition.tester.run_test(float1, float2)))
+        else:
+            self.assertEqual(float1 + float2,
+                             TestBasicAddition.tester.run_test(float1, float2))
+
+    def test_zero(self):
+        for flip_bit in range(23):
+            self.run_test(self.ZERO, self.ZERO, flip_bit)
+            self.run_test(self.ZERO, -self.ZERO, flip_bit)
+            self.run_test(-self.ZERO, self.ZERO, flip_bit)
+            self.run_test(-self.ZERO, -self.ZERO, flip_bit)
+
+            self.run_test(self.ZERO, self.ONE, flip_bit)
+            self.run_test(self.ZERO, -self.ONE, flip_bit)
+            self.run_test(-self.ZERO, self.ONE, flip_bit)
+            self.run_test(-self.ZERO, -self.ONE, flip_bit)
+
+            self.run_test(self.ZERO, self.MIN_SUBNORM, flip_bit)
+            self.run_test(self.ZERO, -self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.ZERO, self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.ZERO, -self.MIN_SUBNORM, flip_bit)
+
+            self.run_test(self.ZERO, self.MAX_SUBNORM, flip_bit)
+            self.run_test(self.ZERO, -self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.ZERO, self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.ZERO, -self.MAX_SUBNORM, flip_bit)
+
+            self.run_test(self.ZERO, self.MIN_NORM, flip_bit)
+            self.run_test(self.ZERO, -self.MIN_NORM, flip_bit)
+            self.run_test(-self.ZERO, self.MIN_NORM, flip_bit)
+            self.run_test(-self.ZERO, -self.MIN_NORM, flip_bit)
+
+            self.run_test(self.ZERO, self.MAX_NORM, flip_bit)
+            self.run_test(self.ZERO, -self.MAX_NORM, flip_bit)
+            self.run_test(-self.ZERO, self.MAX_NORM, flip_bit)
+            self.run_test(-self.ZERO, -self.MAX_NORM, flip_bit)
+
+    def test_one(self):
+        for flip_bit in range(23):
+            self.run_test(self.ONE, self.ZERO, flip_bit)
+            self.run_test(self.ONE, -self.ZERO, flip_bit)
+            self.run_test(-self.ONE, self.ZERO, flip_bit)
+            self.run_test(-self.ONE, -self.ZERO, flip_bit)
+
+            self.run_test(self.ONE, self.ONE, flip_bit)
+            self.run_test(self.ONE, -self.ONE, flip_bit)
+            self.run_test(-self.ONE, self.ONE, flip_bit)
+            self.run_test(-self.ONE, -self.ONE, flip_bit)
+
+            self.run_test(self.ONE, self.MIN_SUBNORM, flip_bit)
+            self.run_test(self.ONE, -self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.ONE, self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.ONE, -self.MIN_SUBNORM, flip_bit)
+
+            self.run_test(self.ONE, self.MAX_SUBNORM, flip_bit)
+            self.run_test(self.ONE, -self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.ONE, self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.ONE, -self.MAX_SUBNORM, flip_bit)
+
+            self.run_test(self.ONE, self.MIN_NORM, flip_bit)
+            self.run_test(self.ONE, -self.MIN_NORM, flip_bit)
+            self.run_test(-self.ONE, self.MIN_NORM, flip_bit)
+            self.run_test(-self.ONE, -self.MIN_NORM, flip_bit)
+
+            self.run_test(self.ONE, self.MAX_NORM, flip_bit)
+            self.run_test(self.ONE, -self.MAX_NORM, flip_bit)
+            self.run_test(-self.ONE, self.MAX_NORM, flip_bit)
+            self.run_test(-self.ONE, -self.MAX_NORM, flip_bit)
+
+    def test_min_subnorm(self):
+        for flip_bit in range(23):
+            self.run_test(self.MIN_SUBNORM, self.ZERO, flip_bit)
+            self.run_test(self.MIN_SUBNORM, -self.ZERO, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, self.ZERO, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, -self.ZERO, flip_bit)
+
+            self.run_test(self.MIN_SUBNORM, self.ONE, flip_bit)
+            self.run_test(self.MIN_SUBNORM, -self.ONE, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, self.ONE, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, -self.ONE, flip_bit)
+
+            self.run_test(self.MIN_SUBNORM, self.MIN_SUBNORM, flip_bit)
+            self.run_test(self.MIN_SUBNORM, -self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, -self.MIN_SUBNORM, flip_bit)
+
+            self.run_test(self.MIN_SUBNORM, self.MAX_SUBNORM, flip_bit)
+            self.run_test(self.MIN_SUBNORM, -self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, -self.MAX_SUBNORM, flip_bit)
+
+            self.run_test(self.MIN_SUBNORM, self.MIN_NORM, flip_bit)
+            self.run_test(self.MIN_SUBNORM, -self.MIN_NORM, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, self.MIN_NORM, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, -self.MIN_NORM, flip_bit)
+
+            self.run_test(self.MIN_SUBNORM, self.MAX_NORM, flip_bit)
+            self.run_test(self.MIN_SUBNORM, -self.MAX_NORM, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, self.MAX_NORM, flip_bit)
+            self.run_test(-self.MIN_SUBNORM, -self.MAX_NORM, flip_bit)
+
+    def test_max_subnorm(self):
+        for flip_bit in range(23):
+            self.run_test(self.MAX_SUBNORM, self.ZERO, flip_bit)
+            self.run_test(self.MAX_SUBNORM, -self.ZERO, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, self.ZERO, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, -self.ZERO, flip_bit)
+
+            self.run_test(self.MAX_SUBNORM, self.ONE, flip_bit)
+            self.run_test(self.MAX_SUBNORM, -self.ONE, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, self.ONE, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, -self.ONE, flip_bit)
+
+            self.run_test(self.MAX_SUBNORM, self.MIN_SUBNORM, flip_bit)
+            self.run_test(self.MAX_SUBNORM, -self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, -self.MIN_SUBNORM, flip_bit)
+
+            self.run_test(self.MAX_SUBNORM, self.MAX_SUBNORM, flip_bit)
+            self.run_test(self.MAX_SUBNORM, -self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, -self.MAX_SUBNORM, flip_bit)
+
+            self.run_test(self.MAX_SUBNORM, self.MIN_NORM, flip_bit)
+            self.run_test(self.MAX_SUBNORM, -self.MIN_NORM, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, self.MIN_NORM, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, -self.MIN_NORM, flip_bit)
+
+            self.run_test(self.MAX_SUBNORM, self.MAX_NORM, flip_bit)
+            self.run_test(self.MAX_SUBNORM, -self.MAX_NORM, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, self.MAX_NORM, flip_bit)
+            self.run_test(-self.MAX_SUBNORM, -self.MAX_NORM, flip_bit)
+
+    def test_min_norm(self):
+        for flip_bit in range(23):
+            self.run_test(self.MIN_NORM, self.ZERO, flip_bit)
+            self.run_test(self.MIN_NORM, -self.ZERO, flip_bit)
+            self.run_test(-self.MIN_NORM, self.ZERO, flip_bit)
+            self.run_test(-self.MIN_NORM, -self.ZERO, flip_bit)
+
+            self.run_test(self.MIN_NORM, self.ONE, flip_bit)
+            self.run_test(self.MIN_NORM, -self.ONE, flip_bit)
+            self.run_test(-self.MIN_NORM, self.ONE, flip_bit)
+            self.run_test(-self.MIN_NORM, -self.ONE, flip_bit)
+
+            self.run_test(self.MIN_NORM, self.MIN_SUBNORM, flip_bit)
+            self.run_test(self.MIN_NORM, -self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.MIN_NORM, self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.MIN_NORM, -self.MIN_SUBNORM, flip_bit)
+
+            self.run_test(self.MIN_NORM, self.MAX_SUBNORM, flip_bit)
+            self.run_test(self.MIN_NORM, -self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.MIN_NORM, self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.MIN_NORM, -self.MAX_SUBNORM, flip_bit)
+
+            self.run_test(self.MIN_NORM, self.MIN_NORM, flip_bit)
+            self.run_test(self.MIN_NORM, -self.MIN_NORM, flip_bit)
+            self.run_test(-self.MIN_NORM, self.MIN_NORM, flip_bit)
+            self.run_test(-self.MIN_NORM, -self.MIN_NORM, flip_bit)
+
+            self.run_test(self.MIN_NORM, self.MAX_NORM, flip_bit)
+            self.run_test(self.MIN_NORM, -self.MAX_NORM, flip_bit)
+            self.run_test(-self.MIN_NORM, self.MAX_NORM, flip_bit)
+            self.run_test(-self.MIN_NORM, -self.MAX_NORM, flip_bit)
+
+    def test_max_norm(self):
+        for flip_bit in range(23):
+            self.run_test(self.MAX_NORM, self.ZERO, flip_bit)
+            self.run_test(self.MAX_NORM, -self.ZERO, flip_bit)
+            self.run_test(-self.MAX_NORM, self.ZERO, flip_bit)
+            self.run_test(-self.MAX_NORM, -self.ZERO, flip_bit)
+
+            self.run_test(self.MAX_NORM, self.ONE, flip_bit)
+            self.run_test(self.MAX_NORM, -self.ONE, flip_bit)
+            self.run_test(-self.MAX_NORM, self.ONE, flip_bit)
+            self.run_test(-self.MAX_NORM, -self.ONE, flip_bit)
+
+            self.run_test(self.MAX_NORM, self.MIN_SUBNORM, flip_bit)
+            self.run_test(self.MAX_NORM, -self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.MAX_NORM, self.MIN_SUBNORM, flip_bit)
+            self.run_test(-self.MAX_NORM, -self.MIN_SUBNORM, flip_bit)
+
+            self.run_test(self.MAX_NORM, self.MAX_SUBNORM, flip_bit)
+            self.run_test(self.MAX_NORM, -self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.MAX_NORM, self.MAX_SUBNORM, flip_bit)
+            self.run_test(-self.MAX_NORM, -self.MAX_SUBNORM, flip_bit)
+
+            self.run_test(self.MAX_NORM, self.MIN_NORM, flip_bit)
+            self.run_test(self.MAX_NORM, -self.MIN_NORM, flip_bit)
+            self.run_test(-self.MAX_NORM, self.MIN_NORM, flip_bit)
+            self.run_test(-self.MAX_NORM, -self.MIN_NORM, flip_bit)
+
+            self.run_test(self.MAX_NORM, self.MAX_NORM, flip_bit)
+            self.run_test(self.MAX_NORM, -self.MAX_NORM, flip_bit)
+            self.run_test(-self.MAX_NORM, self.MAX_NORM, flip_bit)
+            self.run_test(-self.MAX_NORM, -self.MAX_NORM, flip_bit)
 
 if __name__ == "__main__":
     numpy.seterr(over="ignore", invalid="ignore")
